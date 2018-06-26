f432269aa9ece62114cc9f45fe85406d6140dd7a
Christian Benincasa <chrisbenincasa@gmail.com>
2018-06-26 08:40:04 -0400

    Fix map creation

--- src/CertificationClient.ts
+++ src/CertificationClient.ts
@@ -1,5 +1,6 @@
 import { MovieDbApiRequestor } from "./MovieDbClient";
 import * as Model from './model';
+import { Certification } from "./model";
 
 export class CertificationAccessClient {
     requestor: MovieDbApiRequestor
@@ -9,10 +10,18 @@ export class CertificationAccessClient {
     }
 
     async getMovieCertifications() {
-        return this.requestor.makeRequest<Model.CertificationListResponse>('certification/movie/list');
+        return this.requestor.makeRequest<any>('certification/movie/list').then(this.processResponseToMap);
     }
 
     async getTvCertifications() {
-        return this.requestor.makeRequest<Model.CertificationListResponse>('certification/tv/list');
+        return this.requestor.makeRequest<Model.CertificationListResponse>('certification/tv/list').then(this.processResponseToMap);
+    }
+
+    private processResponseToMap(r: any): Map<string, Certification> {
+        const map = new Map<string, Model.Certification>();
+        Object.keys(r.certifications).forEach(key => {
+            map.set(key, r.certifications[key]);
+        });
+        return map;
     }
 }
